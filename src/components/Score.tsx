53997dbaa55fc0a3619cabb2d368fbee8c60ee58
Herson Schluter <schluters@gmail.com>
2021-03-08 20:26:46 -0300

    fix: remove users score == 0

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -21,39 +21,43 @@ export function Score(props): JSX.Element {
     })
   return (
     <ul className={styles.container}>
-      {usersSorted.map((user, idx) => {
-        return (
-          <li key={user.key} className={idx === 0 ? styles.topUser : styles.user}>
-            <div className={styles.position} title={user.user.name}>
-              <strong>{idx + 1}</strong>
-            </div>
-            <div className={styles.info}>
-              <span className={styles.profile}>
-                <img className={styles.avatar} src={user.user.image} alt={user.user.name} />
-                <div>
-                  <strong>{user.user.name}</strong>
-                  <span>
-                    <img src="icons/level.svg" alt="Level" />
-                    Level {user.level}
+      {usersSorted.map(
+        (user, idx): JSX.Element => {
+          if (user.totalxp > 0) {
+            return (
+              <li key={user.key} className={idx === 0 ? styles.topUser : styles.user}>
+                <div className={styles.position} title={user.user.name}>
+                  <strong>{idx + 1}</strong>
+                </div>
+                <div className={styles.info}>
+                  <span className={styles.profile}>
+                    <img className={styles.avatar} src={user.user.image} alt={user.user.name} />
+                    <div>
+                      <strong>{user.user.name}</strong>
+                      <span>
+                        <img src="icons/level.svg" alt="Level" />
+                        Level {user.level}
+                      </span>
+                    </div>
+                  </span>
+                  <span className={styles.score}>
+                    <span>
+                      <p>
+                        <strong>{user.challenges}</strong> completado{user.challenges > 1 && 's'}
+                      </p>
+                    </span>
+                    <span>
+                      <p>
+                        <strong>{user.totalxp}</strong> xp
+                      </p>
+                    </span>
                   </span>
                 </div>
-              </span>
-              <span className={styles.score}>
-                <span>
-                  <p>
-                    <strong>{user.challenges}</strong> completado{user.challenges > 1 && 's'}
-                  </p>
-                </span>
-                <span>
-                  <p>
-                    <strong>{user.totalxp}</strong> xp
-                  </p>
-                </span>
-              </span>
-            </div>
-          </li>
-        )
-      })}
+              </li>
+            )
+          }
+        }
+      )}
     </ul>
   )
 }
