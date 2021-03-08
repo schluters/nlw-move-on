29c11725d6426ae8033e8895e769dd8533b59060
Herson Schluter <schluters@gmail.com>
2021-03-08 20:16:37 -0300

    fix: manifest.json and browserconfig.xml

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Document, { Html, Head, Main, NextScript, DocumentInitialProps, DocumentContext } from 'next/document'
 
 const APP_NAME = 'Move.On'
+const APP_DESCRIPTION =
+  'O MoveOn é uma aplicação com base na técnica Pomodoro, destinada a desenvolvedores para auxiliar no cuidado da sua saúde e postura.'
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     return await Document.getInitialProps(ctx)
@@ -23,47 +25,43 @@ export default class MyDocument extends Document {
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="apple-mobile-web-app-title" content={APP_NAME} />
-          <meta
-            name="description"
-            content="O MoveOn é uma aplicação com base na técnica Pomodoro, destinada a desenvolvedores para auxiliar no cuidado da sua saúde e postura."
-          />
+          <meta name="description" content={APP_DESCRIPTION} />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="msapplication-config" content="static/icons/browserconfig.xml" />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
           <meta name="msapplication-TileColor" content="#e1e1e6" />
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#121214" />
 
-          <link rel="apple-touch-icon" sizes="180x180" href="static/firefox/firefox-general-256-256.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="static/firefox/firefox-general-32-32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="static/firefox/firefox-general-16-16.png" />
+          <link rel="apple-touch-icon" sizes="57x57" href="/images/touch/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/images/touch/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/images/touch/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/images/touch/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/images/touch/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/images/touch/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/images/touch/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/images/touch/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/touch/apple-icon-180x180.png" />
+          
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/touch/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/touch/favicon-16x16.png" />
 
-          <link rel="manifest" href="static/manifest.json" />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/icons/moveon.svg" color="#121214" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
 
           <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:url" content="https://nlw-move-on.vercel.app/" />
           <meta name="twitter:title" content={APP_NAME} />
-          <meta
-            name="twitter:description"
-            content="O MoveOn é uma aplicação com base na técnica Pomodoro, destinada a desenvolvedores para auxiliar no cuidado da sua saúde e postura."
-          />
-          <meta
-            name="twitter:image"
-            content="https://nlw-move-on.vercel.appstatic/android/android-launchericon-192-192.png"
-          />
+          <meta name="twitter:description" content={APP_DESCRIPTION} />
+          <meta name="twitter:image" content="/images/moveon.gif" />
           <meta name="twitter:creator" content="@schluters" />
 
           <meta property="og:type" content="website" />
           <meta property="og:title" content={APP_NAME} />
-          <meta
-            property="og:description"
-            content="O MoveOn é uma aplicação com base na técnica Pomodoro, destinada a desenvolvedores para auxiliar no cuidado da sua saúde e postura."
-          />
+          <meta property="og:description" content={APP_DESCRIPTION} />
           <meta property="og:site_name" content={APP_NAME} />
           <meta property="og:url" content="https://nlw-move-on.vercel.app/" />
-          <meta property="og:image" content="https://nlw-move-on.vercel.appstatic/icons/apple-touch-icon.png" />
+          <meta property="og:image" content="/images/moveon.gif" />
         </Head>
         <body>
           <div id="fb-root"></div>
